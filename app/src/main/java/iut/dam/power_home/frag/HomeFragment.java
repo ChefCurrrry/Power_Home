f9c2fd9e58448cf0aa3ba01a560cfb3562da297c
kichenassamk <k.kichenasssamy@yahoo.com>
2025-02-21 15:23:55 +0100

    A refaire ou continuer : menu de naigation ( toolbar)

--- app/src/main/java/iut/dam/power_home/frag/HomeFragment.java
+++ app/src/main/java/iut/dam/power_home/frag/HomeFragment.java
@@ -15,7 +15,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_habitat, container, false);
+        return inflater.inflate(R.layout.home_fragment, container, false);
     }
 }
 
